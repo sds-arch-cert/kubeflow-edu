31e1ec4b5fa6d691a2d8865b2b55996862beac62
서세일 <red.suh@samsung.com>
2021-03-14 17:07:48 +0000

    TFjob faring에 도커파일 추가

---
+++ 03-fairing/06-TFJob/02.fairing/README.md
@@ -0,0 +1,13 @@
+# TF Job 실습
+## 사전준비
+- Base Image 준비
+  - **Docker 관련 작업은 모두 VM에서 수행**합니다.
+      ```bash
+      git clone https://github.com/sds-arch-cert/kubeflow-edu.git
+      cd /root/kubeflow-edu/03-fairing/06-TFJob/02.fairing
+      ./buildDcoker.sh
+      ```
+
+## 실행
+- tfjob-fairing.ipynb 실행
+- K8s에서 TFJob / Pod 확인    
---
+++ 03-fairing/06-TFJob/02.fairing/buildDocker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 본 스크립트는 Docker build 가능한 환경에서만 실행됩니다.
+# Jupyter Notebook Termial에서는 실행되지 않습니다.
+# VM에서 실행해주세요.
+
+REGISTRY=registry.kube-system.svc.cluster.local:30000
+IMG=mybase
+
+docker build -t ${REGISTRY}/${IMG} .
+docker push ${REGISTRY}/${IMG}
+
+# 레지스트리 확인
+curl http://${REGISTRY}/v2/_catalog
+curl http://${REGISTRY}/v2/${IMG}/tags/list
