e38c695f62febc01b745a7bae16786b8a08e46c0
kwimpkim <98993071+kwimpkim@users.noreply.github.com>
2022-02-18 00:41:00 +0900

    update docker installation

--- 00-install/01-install-microk8s.sh
+++ 00-install/01-install-microk8s.sh
@@ -4,13 +4,19 @@
 echo '
 =================================
 설치버전 및 환경변수 설정
----------------------------------
+---------------------------------'
 KUBE_VERSION=1.21/stable
-'
 
+
+echo '
+=================================
+microk8s 설치
+---------------------------------
+'
 # snap 설치
 sudo apt update
 sudo apt install snapd -y 
+sudo apt install docker.io -y
 
 # microk8s 설치
 # snap 으로 Kubernetes 1.21 버전의 microk8s 를 설치합니다.
@@ -27,6 +33,7 @@ sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.kubectl k
 sudo snap alias microk8s.kubectl mk
 echo source <(mk completion bash | sed "s/kubectl/mk/g") >> ~/.bashrc
+
 
 echo 설정 적용을 위해 ssh console 을 재접속합니다.
 echo switch user 명령 사용시 google-sudoers group 에서 제외되니, 반드시 gcp console 에서 ssh 접속하시기 바랍니다.
